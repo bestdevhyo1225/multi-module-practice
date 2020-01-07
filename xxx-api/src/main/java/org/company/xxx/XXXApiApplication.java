32877d25aab07dd69609114d3e21f22e514a5fc0
janghyoseog <bestdevhyo1225@gmail.com>
2020-01-08 00:22:09 +0900

    [#1] feat - add multi module design
    - 멀티 모듈 설계

---
+++ xxx-api/src/main/java/org/company/xxx/XXXApiApplication.java
@@ -0,0 +1,11 @@
+package org.company.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XXXApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XXXApiApplication.class, args);
+    }
+}
---
+++ xxx-api/src/test/java/org/company/xxx/XXXApiApplicationTest.java
@@ -0,0 +1,12 @@
+package org.company.xxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class XXXApiApplicationTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
